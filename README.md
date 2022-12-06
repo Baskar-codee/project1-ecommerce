55c84c5eb82258f8e2c18b07e4c1bc49c2ad638e
Baskar-codee <119840528+Baskar-codee@users.noreply.github.com>
2022-12-06 11:55:58 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# project1-ecommerce
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-2kj9x4)
---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+
+import App from './App';
+import Search from './header/search';
+import Header from './header/header';
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement);
+
+root.render(
+  <>
+   <App/>
+   <Header />
+   <Search />
+  </>
+);
